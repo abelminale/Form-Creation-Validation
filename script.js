002e76bc2f72c8286f873b32d6bf6fe2581b9871
Abel Berihun <Abel.Berihun@dar.com>
2024-07-19 10:09:38 +0300

    adding all files index,style

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// script.js
+
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('registration-form');
     const feedbackDiv = document.getElementById('form-feedback');
